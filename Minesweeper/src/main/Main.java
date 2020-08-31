6fb4125d6a06bbb717a385d7b9793e6d7d77768e
Name <Name@Name-PC.fritz.box>
2020-08-31 17:16:36 +0200

    Spielfeld mit Buttons in einem Grid Layout, pair programming

--- Minesweeper/src/main/Main.java
+++ Minesweeper/src/main/Main.java
@@ -1,10 +1,54 @@
 package main;
 
-public class Main {
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+
+public class Main extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Main frame = new Main();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
 
+	/**
+	 * Create the frame.
+	 */
+	public Main() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 1192, 690);
+//		setResizable(false);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
+		
+		JPanel center = new Playingfield(3, 3);
+		contentPane.add(center, BorderLayout.CENTER);
+		
+		JPanel top = new JPanel();
+		contentPane.add(top, BorderLayout.PAGE_START);
+		
+		JLabel lblHallo = new JLabel("Hallo");
+		top.add(lblHallo);
 	}
 
 }
---
+++ Minesweeper/src/main/Playingfield.java
@@ -0,0 +1,24 @@
+package main;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import java.awt.GridLayout;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+
+public class Playingfield extends JPanel{
+
+	private JButton[] field;
+	
+	public Playingfield(int width, int height) {
+		
+		field = new JButton[width*height];
+		setLayout(new GridLayout(width, height));
+		
+		for (int i = 0; i < field.length; i++) {
+			field[i] = new JButton("this is button "+ i);
+			this.add(field[i]);
+		}
+	}
+}
