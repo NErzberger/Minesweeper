145440e07603b312111b3a95ac71b48d890db741
I517988 <I517988@WDFN34686446A.emea.global.corp.sap>
2020-10-04 10:50:11 +0200

    alle Felder aufdecken bei Spielende, Pair Programming

--- Minesweeper/src/model/DataGrid.java
+++ Minesweeper/src/model/DataGrid.java
@@ -44,17 +44,8 @@ public class DataGrid {
 			if(positionBombs[randomNumber] == null) {
 				positionBombs[randomNumber] = "x";
 				numberBombs--;
-		
-			}
-			
+			}	
 		}
-		
-//		positionBombs[2] = "x";
-//		positionBombs[7] = "x";
-//		positionBombs[10] = "x";
-//		for(int i=0; i<positionBombs.length; i++) {
-//			System.out.println(positionBombs[i]);
-//		}
 	}
 
 	/**
